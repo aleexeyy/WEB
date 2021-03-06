8bd03e8c1776f3e64fa871f123eb8eff1e014901
student13 <namestudent94@gmail.com>
2021-03-06 14:07:46 +0200

    1st js lesson

---
+++ Scripts/script1.js
@@ -0,0 +1,201 @@
+// x = Number(prompt())
+// if (x > 0) {
+//   x = x +1
+// }
+// console.log(x)
+//---------------------------------------
+// x = Number(prompt())
+// if (x > 0) {
+// x = x+1
+ 
+// }
+// else {
+//   x = x- 2
+
+// }
+// console.log(x)
+//-----------------------------------------
+// x = Number(prompt())
+// if (x > 0) {
+//    x = x +1
+// }
+// else if (x == 0) {
+//    x = 10
+// }
+// else {
+//    x = x -2
+// }
+// console.log(x)
+//--------------------------------------------
+// x = Number(prompt())
+// y = Number(prompt())
+// z = Number(prompt())
+// let count = 0
+// if (x > 0) {
+//    count = count + 1
+// }
+// if (y > 0) {
+//    count = count + 1
+// }
+// if (z > 0) {
+//    count = count +1
+// }
+// console.log(count)
+//-----------------------------------------------
+// x = Number(prompt())
+// y = Number(prompt())
+// z = Number(prompt())
+// let countpol = 0
+// let countotr = 0
+// let countzero = 0
+// if (x > 0) {
+// countpol = countpol + 1
+// }
+// else if (x == 0) {
+//    countzero = countzero + 1
+// }
+// else {
+// countotr = countotr + 1
+// }
+// if (y > 0) {
+//  countpol = countpol + 1
+// }
+// else if (y == 0) {
+//    countzero = countzero + 1
+// }
+// else {
+//    countotr = countotr + 1
+// }
+// if (z > 0) {
+// countpol = countpol + 1
+// }
+// else if (z == 0) {
+//    countzero = countzero + 1
+// }
+// else {
+//    countotr = countotr + 1
+// }
+// console.log("> 0",countpol)
+// console.log(" = 0",countzero)
+// console.log("< 0",countotr)
+//----------------------------------------------
+// x = Number(prompt())
+// y = Number(prompt())
+// if (x == y) {
+//    console.log("equal")
+// }
+// else if (x > y) {
+//    console.log(x)
+// }
+// else if (x < y) {
+//    console.log(y)
+// }
+//-----------------------------------------------
+// x = Number(prompt())
+// y = Number(prompt())
+// if (x == y) {
+//    console.log("equal")
+// }
+// else if (x > y) {
+//    console.log(2)
+// }
+// else if (x < y) {
+//    console.log(1)
+// }
+//----------------------------------------------
+// x = Number(prompt())
+// y = Number(prompt())
+// if (x == y) {
+//    console.log("equal", x, y)
+// }
+// else if (x > y) {
+//    console.log(x, y)
+// }
+// else if (x < y) {
+//    console.log(y, x)
+// }
+//--------------------------------------------
+// a = Number(prompt())
+// b = Number(prompt())
+// let swap = 0
+// if (b > a) {
+//    swap = a
+//    a = b
+//    b = swap
+// }
+// console.log(a)
+// console.log(b)
+//-------------------------------------------
+// a = Number(prompt())
+// b = Number(prompt())
+// let sum = a + b
+// if (a != b) {
+//    a = sum
+//    b = sum
+// }
+// else {
+//    a = 0
+//    b = 0
+// }
+// console.log(a)
+// console.log(b)
+//-----------------------------------------
+// a = Number(prompt())
+// b = Number(prompt())
+// let maxnum = Math.max(a, b)
+// console.log(maxnum)
+// if (a != b) {
+//    a = maxnum
+//    b = maxnum
+// }
+// else {
+//    a = 0
+//    b = 0
+// }
+// console.log(a)
+// console.log(b)
+//----------------------------------------------
+// x = Number(prompt())
+// y = Number(prompt())
+// z = Number(prompt())
+// let ans = Math.min(x, y, z)
+// console.log(ans)
+//------------------------------------------
+// x = Number(prompt())
+// y = Number(prompt())
+// z = Number(prompt())
+// let maxnum = Math.max(x, y, z)
+// let minnum = Math.min(x, y, z)
+// console.log(x+y+z - maxnum - minnum)
+//-----------------------------------------
+// x = Number(prompt())
+// y = Number(prompt())
+// z = Number(prompt())
+// let maxnum = Math.max(x, y, z)
+// let minnum = Math.min(x, y, z)
+// console.log(minnum, maxnum)
+//------------------------------------------
+// x = Number(prompt())
+// y = Number(prompt())
+// z = Number(prompt())
+// let maxnum = Math.max(x, y, z)
+// let minnum = Math.min(x, y, z)
+// console.log(x+y+z - minnum)
+//------------------------------------------
+// x = Number(prompt())
+// y = Number(prompt())
+// z = Number(prompt())
+// let maxnum = Math.max(x, y, z)
+// let minnum = Math.min(x, y, z)
+// if (x == minnum && z == maxnum) {
+//    x *= 2
+//    y *= 2
+//    z *= 2
+// }
+// else {
+//    x = -x
+//    y = -y
+//    z = -z
+// }
+// console.log(x, y, z)
+//--------------------------------
